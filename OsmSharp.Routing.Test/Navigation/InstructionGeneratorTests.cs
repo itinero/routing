f43427c5b0a2376f54ccbedb3e936f4c684f8fa1
xivk <ben@osmsharp.com>
2015-10-25 16:38:00 +0000

    Initial version of instruction generator.

---
+++ OsmSharp.Routing.Test/Navigation/InstructionGeneratorTests.cs
@@ -0,0 +1,101 @@
+﻿// OsmSharp - OpenStreetMap (OSM) SDK
+// Copyright (C) 2015 Abelshausen Ben
+// 
+// This file is part of OsmSharp.
+// 
+// OsmSharp is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 2 of the License, or
+// (at your option) any later version.
+// 
+// OsmSharp is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with OsmSharp. If not, see <http://www.gnu.org/licenses/>.
+
+using NUnit.Framework;
+using OsmSharp.Routing.Navigation;
+using System.Collections.Generic;
+
+namespace OsmSharp.Routing.Test.Navigation
+{
+    /// <summary>
+    /// Contains tests for the instruction generator.
+    /// </summary>
+    [TestFixture]
+    public class InstructionGeneratorTests
+    {
+        /// <summary>
+        /// Tests generating instructions.
+        /// </summary>
+        [Test]
+        public void TestGenerateInstruction()
+        {
+            var route = new Route()
+            {
+                Segments = new List<RouteSegment>(new RouteSegment[]
+                    {
+                        new RouteSegment(),
+                        new RouteSegment(),
+                        new RouteSegment()
+                    }),
+                Tags = new List<RouteTags>(),
+                TotalDistance = 0,
+                TotalTime = 0
+            };
+
+            var generator = new InstructionGenerator<string>(route, (r, i) =>
+            {
+                return string.Format("Instruction {0}.", i);
+            });
+            generator.Run();
+
+            var instructions = generator.Instructions;
+            Assert.IsNotNull(instructions);
+            Assert.AreEqual(3, instructions.Count);
+            Assert.AreEqual(string.Format("Instruction {0}.", 0), instructions[0]);
+            Assert.AreEqual(string.Format("Instruction {0}.", 1), instructions[1]);
+            Assert.AreEqual(string.Format("Instruction {0}.", 2), instructions[2]);
+        }
+
+        /// <summary>
+        /// Tests merging instructions.
+        /// </summary>
+        [Test]
+        public void TestMergeInstructions()
+        {
+            var route = new Route()
+            {
+                Segments = new List<RouteSegment>(new RouteSegment[]
+                    {
+                        new RouteSegment(),
+                        new RouteSegment(),
+                        new RouteSegment()
+                    }),
+                Tags = new List<RouteTags>(),
+                TotalDistance = 0,
+                TotalTime = 0
+            };
+
+            var generator = new InstructionGenerator<string>(route, (r, i) =>
+            {
+                return string.Format("Instruction {0}", i);
+            },
+            (r, i1, i2) =>
+            {
+                return string.Format("Merged instruction: {0} -> {1}",
+                    i1, i2);
+            });
+            generator.Run();
+
+            var instructions = generator.Instructions;
+            Assert.IsNotNull(instructions);
+            Assert.AreEqual(1, instructions.Count);
+            Assert.AreEqual("Merged instruction: Merged instruction: Instruction 0 -> Instruction 1 -> Instruction 2", 
+                instructions[0]);
+        }
+    }
+}
---
+++ OsmSharp.Routing/Navigation/InstructionGenerator.cs
@@ -0,0 +1,99 @@
+﻿// OsmSharp - OpenStreetMap (OSM) SDK
+// Copyright (C) 2015 Abelshausen Ben
+// 
+// This file is part of OsmSharp.
+// 
+// OsmSharp is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 2 of the License, or
+// (at your option) any later version.
+// 
+// OsmSharp is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with OsmSharp. If not, see <http://www.gnu.org/licenses/>.
+
+using OsmSharp.Routing.Algorithms;
+using System;
+using System.Collections.Generic;
+
+namespace OsmSharp.Routing.Navigation
+{
+    /// <summary>
+    /// An instruction generator.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class InstructionGenerator<T> : AlgorithmBase
+    {
+        private readonly Route _route;
+        private readonly Func<Route, int, T> _getInstruction;
+        private readonly Func<Route, T, T, T> _merge;
+
+        /// <summary>
+        /// Creates a new instruction generator.
+        /// </summary>
+        public InstructionGenerator(Route route, Func<Route, int, T> getInstruction)
+            : this(route, getInstruction, null)
+        {
+
+        }
+
+        /// <summary>
+        /// Creates a new instruction generator.
+        /// </summary>
+        public InstructionGenerator(Route route, Func<Route, int, T> getInstruction,
+            Func<Route, T, T, T> merge)
+        {
+            _route = route;
+            _getInstruction = getInstruction;
+            _merge = merge;
+        }
+
+        private List<T> _instructions;
+
+        /// <summary>
+        /// Executes the actual run of the algorithm.
+        /// </summary>
+        protected override void DoRun()
+        {
+            _instructions = new List<T>();
+
+            for (var i = 0; i < _route.Segments.Count; i++)
+            { // check each part of the route to check for an instruction.
+                var instruction = _getInstruction(_route, i);
+                if (instruction != null)
+                {
+                    _instructions.Add(instruction);
+                }
+            }
+
+            if (_merge != null)
+            {
+                for (var i = 1; i < _instructions.Count; i++)
+                { // keep on merging until impossible.
+                    var merged = _merge(_route, _instructions[i - 1], _instructions[i]);
+                    if (merged != null)
+                    { // ok, an instruction got merged.
+                        _instructions[i - 1] = merged;
+                        _instructions.RemoveAt(i);
+                        i = i - 1; // retry again.
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Gets the generated list of instructions.
+        /// </summary>
+        public List<T> Instructions
+        {
+            get
+            {
+                return _instructions;
+            }
+        }
+    }
+}
