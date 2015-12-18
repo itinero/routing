21a8762410796aeab45a2661c7ba5f9a616c36da
xivk <ben@osmsharp.com>
2015-12-18 17:07:49 +0100

    Updated OsmSharp.Core.

--- OsmSharp.Routing/Data/EdgeData.cs
+++ OsmSharp.Routing/Data/EdgeData.cs
@@ -16,7 +16,6 @@
 // You should have received a copy of the GNU General Public License
 // along with OsmSharp. If not, see <http://www.gnu.org/licenses/>.
 
-using OsmSharp.Collections.Arrays;
 using OsmSharp.Math.Geo.Simple;
 using OsmSharp.Routing.Graphs;
 using System;
