c574353805135b4a9a8961ad9d8add4bace8ca24
xivk <ben@osmsharp.com>
2016-03-02 13:51:48 +0000

    First version of an edge-based dykstra.

---
+++ Itinero/Algorithms/Default/Edge/Dykstra.cs
@@ -0,0 +1,247 @@
+﻿// Itinero - OpenStreetMap (OSM) SDK
+// Copyright (C) 2016 Abelshausen Ben
+// 
+// This file is part of Itinero.
+// 
+// Itinero is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 2 of the License, or
+// (at your option) any later version.
+// 
+// Itinero is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with Itinero. If not, see <http://www.gnu.org/licenses/>.
+
+using Itinero.Algorithms.PriorityQueues;
+using Itinero.Data;
+using Itinero.Graphs;
+using Itinero.Profiles;
+using System;
+using System.Collections.Generic;
+
+namespace Itinero.Algorithms.Default.Edge
+{
+    /// <summary>
+    /// An implementation of the dykstra routing algorithm.
+    /// </summary>
+    public class Dykstra : AlgorithmBase
+    {
+        private readonly Graph _graph;
+        private readonly IEnumerable<EdgePath> _sources;
+        private readonly Func<ushort, Factor> _getFactor;
+        private readonly float _sourceMax;
+        private readonly bool _backward;
+
+        /// <summary>
+        /// Creates a new one-to-all dykstra algorithm instance.
+        /// </summary>
+        public Dykstra(Graph graph, Func<ushort, Factor> getFactor,
+            IEnumerable<EdgePath> sources, float sourceMax, bool backward)
+        {
+            _graph = graph;
+            _sources = sources;
+            _getFactor = getFactor;
+            _sourceMax = sourceMax;
+            _backward = backward;
+        }
+
+        private Graph.EdgeEnumerator _edgeEnumerator;
+        private Dictionary<long, EdgePath> _visits;
+        private EdgePath _current;
+        private BinaryHeap<EdgePath> _heap;
+        private Dictionary<uint, Factor> _factors;
+
+        /// <summary>
+        /// Executes the algorithm.
+        /// </summary>
+        protected override void DoRun()
+        {
+            // initialize stuff.
+            this.Initialize();
+
+            // start the search.
+            while (this.Step()) { }
+        }
+
+        /// <summary>
+        /// Initializes and resets.
+        /// </summary>
+        public void Initialize()
+        {
+            // algorithm always succeeds, it may be dealing with an empty network and there are no targets.
+            this.HasSucceeded = true;
+
+            // initialize a dictionary of speeds per edge profile.
+            _factors = new Dictionary<uint, Factor>();
+
+            // intialize dykstra data structures.
+            _visits = new Dictionary<long, EdgePath>();
+            _heap = new BinaryHeap<EdgePath>(1000);
+
+            // queue all sources.
+            foreach (var source in _sources)
+            {
+                _heap.Push(source, source.Weight);
+            }
+
+            // gets the edge enumerator.
+            _edgeEnumerator = _graph.GetEdgeEnumerator();
+        }
+
+        /// <summary>
+        /// Executes one step in the search.
+        /// </summary>
+        public bool Step()
+        {
+            // while the visit list is not empty.
+            _current = null;
+            if (_heap.Count > 0)
+            { // choose the next vertex.
+                _current = _heap.Pop();
+                while (_current != null && _visits.ContainsKey(_current.DirectedEdge))
+                { // keep dequeuing.
+                    if (_heap.Count == 0)
+                    { // nothing more to pop.
+                        break;
+                    }
+                    _current = _heap.Pop();
+                }
+            }
+
+            if (_current != null &&
+                !_visits.ContainsKey(_current.DirectedEdge))
+            { // we visit this one, set visit.
+                _visits[_current.DirectedEdge] = _current;
+            }
+            else
+            { // route is not found, there are no vertices left
+                // or the search went outside of the max bounds.
+                return false;
+            }
+
+            // get neighbouring edges at target vertex.
+            _edgeEnumerator.MoveToTargetVertex(_current.DirectedEdge);
+            while (_edgeEnumerator.MoveNext())
+            {
+                var edge = _edgeEnumerator;
+                var directedEdgeId = _edgeEnumerator.IdDirected();
+
+                if (directedEdgeId == -_current.DirectedEdge)
+                { // don't go back.
+                    continue;
+                }
+
+                if (_visits.ContainsKey(directedEdgeId))
+                { // has already been choosen.
+                    continue;
+                }
+
+                // get the speed from cache or calculate.
+                float distance;
+                ushort edgeProfile;
+                EdgeDataSerializer.Deserialize(edge.Data0, out distance, out edgeProfile);
+                var factor = Factor.NoFactor;
+                if (!_factors.TryGetValue(edgeProfile, out factor))
+                { // speed not there, calculate speed.
+                    factor = _getFactor(edgeProfile);
+                    _factors.Add(edgeProfile, factor);
+                }
+
+                // check the tags against the interpreter.
+                if (factor.Value > 0 && (factor.Direction == 0 ||
+                    (!_backward && (factor.Direction == 1) != edge.DataInverted) ||
+                    (_backward && (factor.Direction == 1) == edge.DataInverted)))
+                { // it's ok; the edge can be traversed by the given vehicle.
+                    // calculate neighbors weight.
+                    var totalWeight = _current.Weight + (distance * factor.Value);
+                    if (totalWeight < _sourceMax)
+                    { // update the visit list.
+                        _heap.Push(new EdgePath(directedEdgeId, totalWeight, _current),
+                            totalWeight);
+                    }
+                }
+            }
+            return true;
+        }
+
+        /// <summary>
+        /// Sets a visit on a vertex from an external source (like a transit-algorithm).
+        /// </summary>
+        /// <remarks>The algorithm will pick up these visits as if it was one it's own.</remarks>
+        /// <returns>True if the visit was set successfully.</returns>
+        public bool SetVisit(EdgePath visit)
+        {
+            if (!_visits.ContainsKey(visit.DirectedEdge))
+            {
+                _heap.Push(visit, visit.Weight);
+                return true;
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// Returns true if the given edge was visited and sets the visit output parameters with the actual visit data.
+        /// </summary>
+        public bool TryGetVisit(long edge, out EdgePath visit)
+        {
+            return _visits.TryGetValue(edge, out visit);
+        }
+
+        /// <summary>
+        /// Gets the max reached flag.
+        /// </summary>
+        /// <remarks>True if the source-max value was reached.</remarks>
+        public bool MaxReached { get; private set; }
+
+        /// <summary>
+        /// The was edge found delegate.
+        /// </summary>
+        public delegate bool WasEdgeFoundDelegate(uint vertex, uint edge, float weight);
+
+        /// <summary>
+        /// Gets or sets the wasfound function to be called when a new vertex is found.
+        /// </summary>
+        public WasEdgeFoundDelegate WasEdgeFound
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Gets the backward flag.
+        /// </summary>
+        public bool Backward
+        {
+            get
+            {
+                return _backward;
+            }
+        }
+
+        /// <summary>
+        /// Gets the graph.
+        /// </summary>
+        public Graph Graph
+        {
+            get
+            {
+                return _graph;
+            }
+        }
+
+        /// <summary>
+        /// Gets the current.
+        /// </summary>
+        public EdgePath Current
+        {
+            get
+            {
+                return _current;
+            }
+        }
+    }
+}
---
+++ Itinero/Graphs/EdgeExtensions.cs
@@ -0,0 +1,95 @@
+﻿// Itinero - OpenStreetMap (OSM) SDK
+// Copyright (C) 2015 Abelshausen Ben
+// 
+// This file is part of Itinero.
+// 
+// Itinero is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 2 of the License, or
+// (at your option) any later version.
+// 
+// Itinero is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with Itinero. If not, see <http://www.gnu.org/licenses/>.
+
+using System;
+
+namespace Itinero.Graphs
+{
+    /// <summary>
+    /// Contains extensions related to edges, edge id's and directed edges.
+    /// </summary>
+    public static class EdgeExtensions
+    {
+        /// <summary>
+        /// Returns a directed version of the edge-id. Smaller than 0 if inverted, as-is if not inverted.
+        /// </summary>
+        /// <remarks>
+        /// The relationship between a regular edge id and a directed edge id:
+        /// - 0 -> 1 forward, -1 backward.
+        /// - all other id's are offset by 1 and postive when forward, negative when backward.
+        /// </remarks>
+        public static long IdDirected(this Graph.EdgeEnumerator enumerator)
+        {
+            if (enumerator.DataInverted)
+            {
+                return -(enumerator.Id + 1);
+            }
+            return (enumerator.Id + 1);
+        }
+
+        /// <summary>
+        /// Moves to the given directed edge-id.
+        /// </summary>
+        public static void MoveToEdge(this Graph.EdgeEnumerator enumerator, long directedEdgeId)
+        {
+            if (directedEdgeId == 0) { throw new ArgumentOutOfRangeException("directedEdgeId"); }
+
+            uint edgeId;
+            if (directedEdgeId > 0)
+            {
+                edgeId = (uint)directedEdgeId - 1;
+            }
+            else
+            {
+                edgeId = (uint)((-directedEdgeId) - 1);
+            }
+            enumerator.MoveToEdge(edgeId);
+        }
+
+        /// <summary>
+        /// Moves this enumerator to the target vertex of the given directed edge-id.
+        /// </summary>
+        public static void MoveToTargetVertex(this Graph.EdgeEnumerator enumerator, long directedEdgeId)
+        {
+            enumerator.MoveToEdge(directedEdgeId);
+
+            if (directedEdgeId > 0)
+            {
+                if (!enumerator.DataInverted)
+                {
+                    enumerator.MoveTo(enumerator.To);
+                }
+                else
+                {
+                    enumerator.MoveTo(enumerator.From);
+                }
+            }
+            else
+            {
+                if (enumerator.DataInverted)
+                {
+                    enumerator.MoveTo(enumerator.To);
+                }
+                else
+                {
+                    enumerator.MoveTo(enumerator.From);
+                }
+            }
+        }
+    }
+}
