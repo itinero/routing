f38f5559b4f1408fa7a49b02dce847fc37063481
xivk <ben@osmsharp.com>
2016-07-25 17:20:47 +0100

    Updated to 0.12.

--- SharedAssemblyVersion.cs
+++ SharedAssemblyVersion.cs
@@ -18,5 +18,5 @@
 
 using System.Reflection;
 
-[assembly: AssemblyVersion("0.11.6")] // semantic versioning Major.Minor.Patch
-[assembly: AssemblyInformationalVersion("0.11.6-rc1")]
+[assembly: AssemblyVersion("0.12.0")] // semantic versioning Major.Minor.Patch
+[assembly: AssemblyInformationalVersion("0.12.0-rc1")]
