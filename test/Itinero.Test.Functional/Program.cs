c0bdfde0afac630125b330ea504c584a55a120a4
ben.abelshausen <ben@osmsharp.com>
2016-11-01 11:22:57 +0100

    Updated nuspec and fixed functional tests.

--- test/Itinero.Test.Functional/Program.cs
+++ test/Itinero.Test.Functional/Program.cs
@@ -77,22 +77,22 @@ namespace Itinero.Test.Functional
 
             Runner.GetTestAddContracted(routerDb, Vehicle.Car.Fastest(), true).TestPerf("Add contracted graph for Car.Fastest() edge based");
 
-            //// TEST1: Test random routes.
-            //Runner.GetTestRandomRoutes(new Router(routerDb), Vehicle.Car.Fastest(), 100).TestPerf("Testing route calculation speed.");
+            // TEST1: Test random routes.
+            Runner.GetTestRandomRoutes(new Router(routerDb), Vehicle.Car.Fastest(), 100).TestPerf("Testing route calculation speed.");
 
-            //// TEST2: Tests find islands.
-            //Runner.GetTestIslandDetection(routerDb).TestPerf("Testing island detection.", 10);
+            // TEST2: Tests find islands.
+            Runner.GetTestIslandDetection(routerDb).TestPerf("Testing island detection.", 10);
 
-            //// TEST3: calulate isochrones.
-            //var polygons = Runner.GetTestIsochroneCalculation(router).TestPerf("Testing isochrone calculation.", 1);
-            //var polygonsJson = polygons.ToFeatureCollection().ToGeoJson();
+            // TEST3: calulate isochrones.
+            var polygons = Runner.GetTestIsochroneCalculation(router).TestPerf("Testing isochrone calculation.", 1);
+            var polygonsJson = polygons.ToFeatureCollection().ToGeoJson();
 
-            //// TEST4: calculate heatmaps.
-            //var heatmap = Runner.GetTestHeatmapCalculation(router).TestPerf("Testing heatmap calculation.", 10);
+            // TEST4: calculate heatmaps.
+            var heatmap = Runner.GetTestHeatmapCalculation(router).TestPerf("Testing heatmap calculation.", 10);
 
-            //// TEST5: calculate tree.
-            //var lines = Runner.GetTestTreeCalculation(router).TestPerf("Testing tree calculation.", 100);
-            //var linesJson = lines.ToFeatureCollection().ToGeoJson();
+            // TEST5: calculate tree.
+            var lines = Runner.GetTestTreeCalculation(router).TestPerf("Testing tree calculation.", 100);
+            var linesJson = lines.ToFeatureCollection().ToGeoJson();
 
             // TEST6: calculate many to many routes.
             //Runner.GetTestAddContracted(routerDb, Vehicle.Car.Fastest(), false).TestPerf("Add contracted graph for Car.Fastest() vertex based");
