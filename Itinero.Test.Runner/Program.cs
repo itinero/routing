df0c7e49c85c6a4327b46fbfe86225be2ef66c8f
ben.abelshausen <ben@osmsharp.com>
2017-03-14 11:02:59 +0100

    Added test runner project.

---
+++ Itinero.Test.Runner/Program.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Common;
+using NUnitLite;
+using System;
+using System.Reflection;
+
+namespace Itinero.Test.Runner
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            var res = new AutoRun(typeof(RouterPointTests).GetTypeInfo().Assembly)
+                .Execute(args, new ExtendedTextWrapper(Console.Out), Console.In);
+
+#if DEBUG
+            Console.ReadLine();
+#endif
+            return res;
+        }
+    }
+}
