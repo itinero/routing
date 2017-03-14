6f6c5bf01d64d4d6102e66e1ec75e7bb1ca237e8
ben.abelshausen <ben@osmsharp.com>
2017-03-14 11:54:42 +0100

    Added windows build script.

---
+++ build.bat
@@ -0,0 +1,12 @@
+dotnet build ./src/Itinero
+dotnet build ./src/Itinero.Geo
+dotnet build ./src/Itinero.IO.Osm
+dotnet build ./src/Itinero.IO.Shape
+
+dotnet build ./test/Itinero.Test
+dotnet build ./test/Itinero.Test.Functional
+dotnet build ./test/Itinero.Test.Runner
+
+dotnet build ./samples/Sample.Basic
+dotnet build ./samples/Sample.Matrix
+dotnet build ./samples/Sample.Shape
