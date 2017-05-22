6c98567da3173423085c592acda1c9b31d64ea21
Ben Abelshausen <ben@osmsharp.com>
2017-05-22 09:07:50 +0200

    Added definition of original edge and turn.

---
+++ src/Itinero/Algorithms/OriginalEdge.cs
@@ -0,0 +1,117 @@
+﻿/*
+ *  Licensed to SharpSoftware under one or more contributor
+ *  license agreements. See the NOTICE file distributed with this work for 
+ *  additional information regarding copyright ownership.
+ * 
+ *  SharpSoftware licenses this file to you under the Apache License, 
+ *  Version 2.0 (the "License"); you may not use this file except in 
+ *  compliance with the License. You may obtain a copy of the License at
+ * 
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+using Itinero.Algorithms.Contracted.EdgeBased;
+using Itinero.Graphs.Directed;
+
+namespace Itinero.Algorithms
+{
+    /// <summary>
+    /// Represents an original edge consisting of vertex1 -> vertex2.
+    /// </summary>
+    public struct OriginalEdge
+    {
+        /// <summary>
+        /// Creates an original edge.
+        /// </summary>
+        public OriginalEdge(uint vertex1, uint vertex2)
+        {
+            this.Vertex1 = vertex1;
+            this.Vertex2 = vertex2;
+        }
+
+        /// <summary>
+        /// Gets or sets vertex1.
+        /// </summary>
+        public uint Vertex1 { get; set; }
+
+        /// <summary>
+        /// Gets or sets vertex2.
+        /// </summary>
+        public uint Vertex2 { get; set; }
+
+        /// <summary>
+        /// Converts this original edge to an edge path.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        public EdgePath<T> ToEdgePath<T>(T weight = default(T))
+        {
+            return new EdgePath<T>(this.Vertex2, weight, new EdgePath<T>(this.Vertex1));
+        }
+
+        /// <summary>
+        /// Gets a hash.
+        /// </summary>
+        /// <returns></returns>
+        public override int GetHashCode()
+        {
+            return this.Vertex1.GetHashCode() ^
+                this.Vertex2.GetHashCode();
+        }
+
+        /// <summary>
+        /// Returns true if the given object represents the same edge.
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public override bool Equals(object obj)
+        {
+            if (obj is OriginalEdge)
+            {
+                return this.Equals((OriginalEdge)obj);
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// Returns true if the given object represents the same edge.
+        /// </summary>
+        public bool Equals(OriginalEdge obj)
+        {
+            return obj.Vertex1 == this.Vertex1 &&
+                obj.Vertex2 == this.Vertex2;
+        }
+
+        /// <summary>
+        /// Reverses this edge.
+        /// </summary>
+        /// <returns></returns>
+        public OriginalEdge Reverse()
+        {
+            return new OriginalEdge(this.Vertex2, this.Vertex1);
+        }
+
+        /// <summary>
+        /// Returns a description.
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            return string.Format("{0}->{1}", this.Vertex1, this.Vertex2);
+        }
+    }
+
+    /// <summary>
+    /// Contains origin edge related extension methods.
+    /// </summary>
+    public static class OriginalEdgeExtensions
+    {
+
+    }
+}
---
+++ src/Itinero/Algorithms/Turn.cs
@@ -0,0 +1,169 @@
+﻿/*
+ *  Licensed to SharpSoftware under one or more contributor
+ *  license agreements. See the NOTICE file distributed with this work for 
+ *  additional information regarding copyright ownership.
+ * 
+ *  SharpSoftware licenses this file to you under the Apache License, 
+ *  Version 2.0 (the "License"); you may not use this file except in 
+ *  compliance with the License. You may obtain a copy of the License at
+ * 
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+using Itinero.Algorithms.Restrictions;
+using System;
+
+namespace Itinero.Algorithms
+{
+    /// <summary>
+    /// Represents a turn.
+    /// </summary>
+    public struct Turn
+    {
+        /// <summary>
+        /// Creates a new turn.
+        /// </summary>
+        /// <param name="vertex1"></param>
+        /// <param name="vertex2"></param>
+        /// <param name="vertex3"></param>
+        public Turn(uint vertex1, uint vertex2, uint vertex3)
+        {
+            this.Vertex1 = vertex1;
+            this.Vertex2 = vertex2;
+            this.Vertex3 = vertex3;
+        }
+
+        /// <summary>
+        /// Creates a turn from an original and a third vertex.
+        /// </summary>
+        public Turn(OriginalEdge edge, uint to)
+        {
+            this.Vertex1 = edge.Vertex1;
+            this.Vertex2 = edge.Vertex2;
+            this.Vertex3 = to;
+        }
+
+        /// <summary>
+        /// First vertex.
+        /// </summary>
+        public uint Vertex1 { get; set; }
+
+        /// <summary>
+        /// Second vertex.
+        /// </summary>
+        public uint Vertex2 { get; set; }
+
+        /// <summary>
+        /// Third vertex.
+        /// </summary>
+        public uint Vertex3 { get; set; }
+
+        /// <summary>
+        /// Gets the n-th vertex in this turn.
+        /// </summary>
+        /// <param name="i"></param>
+        /// <returns></returns>
+        public uint this[int i]
+        {
+            get
+            {
+                switch (i)
+                {
+                    case 0:
+                        return this.Vertex1;
+                    case 1:
+                        return this.Vertex2;
+                    case 2:
+                        return this.Vertex3;
+                }
+                throw new IndexOutOfRangeException();
+            }
+        }
+
+        /// <summary>
+        /// Reverses this turn.
+        /// </summary>
+        /// <returns></returns>
+        public void Reverse()
+        {
+            var t = this.Vertex3;
+            this.Vertex3 = this.Vertex1;
+            this.Vertex1 = t;
+        }
+
+        /// <summary>
+        /// Returns true if this turn is restricted by one of the restrictions in the given collection.
+        /// </summary>
+        public bool IsRestrictedBy(RestrictionCollection restrictions)
+        {
+            for (var r = 0; r < restrictions.Count; r++)
+            {
+                if (IsRestrictedBy(restrictions[r]))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// Returns true if this turn is restricted by the given restriction.
+        /// </summary>
+        public bool IsRestrictedBy(Restriction restriction)
+        {
+            if (restriction.Vertex3 == Constants.NO_VERTEX)
+            {
+                if (restriction.Vertex2 == Constants.NO_VERTEX)
+                {
+                    return restriction.Vertex1 == this.Vertex1 ||
+                        restriction.Vertex1 == this.Vertex2 ||
+                        restriction.Vertex1 == this.Vertex3;
+                }
+                return (restriction.Vertex1 == this.Vertex1 &&
+                    restriction.Vertex2 == this.Vertex2) ||
+                    (restriction.Vertex1 == this.Vertex2 &&
+                    restriction.Vertex2 == this.Vertex3);
+            }
+            return restriction.Vertex1 == this.Vertex1 &&
+                restriction.Vertex2 == this.Vertex2 &&
+                restriction.Vertex3 == this.Vertex3;
+        }
+
+        /// <summary>
+        /// Gets the length of this turn.
+        /// </summary>
+        public int Length
+        {
+            get
+            {
+                return 3;
+            }
+        }
+
+        /// <summary>
+        /// Returns true if this turn is a u-turn.
+        /// </summary>
+        public bool IsUTurn
+        {
+            get
+            {
+                return this.Vertex1 == this.Vertex3;
+            }
+        }
+
+        /// <summary>
+        /// Returns a description of this turn.
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            return string.Format("{0}->{1}->{2}", this.Vertex1, this.Vertex2, this.Vertex3);
+        }
+    }
+}
