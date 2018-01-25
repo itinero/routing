b5b514b978473374b7e7a81009fd4375be761053
xivk <ben@itinero.tech>
2018-01-25 13:58:05 +0100

    Updated vscode configurations and build scripts.

--- build.sh
+++ build.sh
@@ -6,12 +6,6 @@ dotnet build ./test/Itinero.Test
 dotnet build ./test/Itinero.Test.Functional
 
 # Build samples.
-dotnet build ./samples/Sample.Basic -r ubuntu.16.04-x64
-dotnet build ./samples/Sample.Basic -r win10-x64
-dotnet build ./samples/Sample.Basic -r osx.10.11-x64
-dotnet build ./samples/Sample.Matrix -r ubuntu.16.04-x64
-dotnet build ./samples/Sample.Matrix -r win10-x64
-dotnet build ./samples/Sample.Matrix -r osx.10.11-x64
-dotnet build ./samples/Sample.Elevation -r ubuntu.16.04-x64
-dotnet build ./samples/Sample.Elevation -r win10-x64
-dotnet build ./samples/Sample.Elevation -r osx.10.11-x64
+dotnet build ./samples/Sample.Basic
+dotnet build ./samples/Sample.Matrix
+dotnet build ./samples/Sample.Elevation
