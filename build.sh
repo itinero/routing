ceb4e342080e038526e09ce642e022fd215778ab
xivk <ben@itinero.tech>
2018-01-25 13:28:16 +0100

    Fixed build script.

--- build.sh
+++ build.sh
@@ -2,9 +2,8 @@ dotnet build ./src/Itinero -f netstandard1.3
 dotnet build ./src/Itinero.IO.Osm -f netstandard1.6
 
 # Waiting to port to .NET core.
-dotnet build ./test/Itinero.Test -f netstandard1.3
-dotnet build ./test/Itinero.Test.Functional -f netstandard1.3
-dotnet build ./test/Itinero.Test.Runner -f netstandard1.3
+dotnet build ./test/Itinero.Test
+dotnet build ./test/Itinero.Test.Functional
 
 # Build samples.
 dotnet build ./samples/Sample.Basic -r ubuntu.16.04-x64
