6c7ace3c0bcda00b654431ab46ae0a7a09a5c0d5
Pietervdvn <pietervdvn@gmail.com>
2018-05-30 17:57:29 +0200

    Calculating surfacee area

--- src/Itinero/LocalGeo/Operations/PolygonAreaCalcutor.cs
+++ src/Itinero/LocalGeo/Operations/PolygonAreaCalcutor.cs
@@ -1,11 +1,30 @@
-﻿namespace Itinero.LocalGeo.Operations
+﻿using System.Collections.Generic;
+
+namespace Itinero.LocalGeo.Operations
 {
-    
     /// <summary>
     ///  Calculates the surface areas of polygons
     /// </summary>
-    internal class PolygonAreaCalcutor
+    internal static class PolygonAreaCalcutor
     {
-        
+        /// <summary>
+        /// Calculates the surface area of a closed, not-self-intersecting polygon
+        /// </summary>
+        /// <param name="points"></param>
+        /// <returns></returns>
+        internal static float surfaceOf(this List<Coordinate> points)
+        {
+            var l = points.Count;
+            var area = 0f;
+            for (var i = 1; i < l+1; i++)
+            {
+                var p = points[i];
+                var pi = points[(i + 1) % l];
+                var pm = points[(i - 1)];
+                area += p.Longitude * (pi.Latitude - pm.Latitude);
+            }
+
+            return area / 2;
+        }
     }
 }
---
+++ test/Itinero.Test/LocalGeo/Operations/PolygonAreaCalculatorTest.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+
+namespace Itinero.Test.LocalGeo.Operations
+{
+    [TestFixture]
+    public class PolygonAreaCalculatorTest
+    {
+
+        [Test]
+        public void TestSimpleArea()
+        {
+            "Itinero.Test.test-data.points.points1.hull.geojson"
+        }
+        
+    }
+}
