873a8b529f65c4e10154837b5788390cd7e73ff5
xivk <ben@itinero.tech>
2018-05-31 14:32:14 +0100

    More edge-based label work.

--- src/Itinero/Algorithms/Networks/Islands/EdgeBasedIslandDetector.cs
+++ src/Itinero/Algorithms/Networks/Islands/EdgeBasedIslandDetector.cs
@@ -20,9 +20,13 @@ using System;
 using System.Collections.Generic;
 using System.Threading;
 using Itinero.Algorithms.Contracted;
+using Itinero.Algorithms.Networks.Analytics.Heatmaps;
 using Itinero.Data.Network;
+using Itinero.Data.Network.Edges;
 using Itinero.Graphs;
+using Itinero.Graphs.Directed;
 using Itinero.Profiles;
+using Itinero.Profiles.Lua.Tree;
 using Reminiscence.Arrays;
 
 namespace Itinero.Algorithms.Networks.Islands
@@ -32,61 +36,25 @@ namespace Itinero.Algorithms.Networks.Islands
     /// </summary>
     public class EdgeBasedIslandDetector : AlgorithmBase
     {
-        private readonly RouterDb _routerDb;
+        private readonly RoutingNetwork _network;
         private readonly Func<ushort, Factor> _profile;
 
         /// <summary>
         /// Creates a new island detector.
         /// </summary>
-        /// <param name="routerDb">The router db.</param>
+        /// <param name="network">The network.</param>
         /// <param name="profile">The profile.</param>
-        public EdgeBasedIslandDetector(RouterDb routerDb, Func<ushort, Factor> profile)
+        public EdgeBasedIslandDetector(RoutingNetwork network, Func<ushort, Factor> profile)
         {
-            _routerDb = routerDb;
+            _network = network;
             _profile = profile;
-            
-            _islandGraph = new Graph(1);
         }
 
         // defines some special values that shouldn't be used as island id's.
-        private readonly uint NotSet = uint.MaxValue;
-        private readonly uint NoAccess = uint.MaxValue - 1;
+        public readonly uint NotSet = uint.MaxValue;
+        public readonly uint NoAccess = uint.MaxValue - 1;
         
-        private readonly Graph _islandGraph; // connects islands by id.
         private MemoryArray<uint> _islandIds; // island id's per edge id.
-
-        private uint GetIslandId(DirectedEdgeId edge)
-        {
-            var rawId = edge.Raw;
-            if (_islandIds.Length <= rawId)
-            {
-                var length = _islandIds.Length;
-                _islandIds.EnsureMinimumSize(rawId);
-                for (var i = length; i < _islandIds.Length; i++)
-                {
-                    _islandIds[i] = NotSet;
-                }
-            }
-
-            return _islandIds[rawId];
-        }
-
-        private void SetIslandId(DirectedEdgeId edge, uint islandId)
-        {
-            _islandIds[edge.Raw] = islandId;
-        }
-
-        private uint NewIslandId()
-        {
-            var newId = _islandGraph.VertexCount;
-            _islandGraph.AddVertex(newId);
-            return newId;
-        }
-
-        private uint ConnectIslands(uint island1Id, uint island2Id)
-        {
-            
-        }
 
         /// <summary>
         /// Runs the island detection.
@@ -94,155 +62,182 @@ namespace Itinero.Algorithms.Networks.Islands
         protected override void DoRun(CancellationToken cancellationToken)
         {
             _islandIds = new MemoryArray<uint>(1);
-            uint nextIslandId = 0;
-            
-            var network = _routerDb.Network;
-            var edgeEnumerator1 = network.GetEdgeEnumerator();
-            var edgeEnumerator2 = network.GetEdgeEnumerator();
-            var usedAsStart = new HashSet<uint>();
-            for (uint v = 0; v < network.VertexCount; v++)
+
+            var islandLabels = new IslandLabels();
+
+            // do a run over each vertex and connect islands with bidirectional edges where possible.
+            uint currentVertex = 0;
+            var edgeEnumerator1 = _network.GetEdgeEnumerator();
+            var edgeEnumerator2 = _network.GetEdgeEnumerator();
+            var bestLabels = new Dictionary<uint, uint>();
+            while (currentVertex < _network.VertexCount)
             {
-                if (!edgeEnumerator1.MoveTo(v))
+                if (!edgeEnumerator1.MoveTo(currentVertex))
                 {
                     continue;
                 }
-                edgeEnumerator2.MoveTo(v);
 
-                // mark edge using it's neigbours.
-                while (true)
+                // log all connections.
+                bestLabels.Clear();
+                while (edgeEnumerator1.MoveNext())
                 {
-                    // find the edge with the lowest island id as a start.
-                    while (edgeEnumerator1.MoveNext())
+                    var edge1Factor = _profile(edgeEnumerator1.Data.Profile);
+                    if (edge1Factor.Value == 0)
                     {
-                        var edge1Id = edgeEnumerator1.Id;
-                        var edge1BackwardId = edgeEnumerator1.DirectedEdgeId(); // from current vertex -> neighbour.
-                        var edge1ForwardId = edge1BackwardId.Reverse; // from neighbour -> current vertex.
-                        
-                        // get edge factor.
-                        var edge1Factor = _profile(edgeEnumerator1.Data.Profile);
-                        
-                        // evaluate forward.
-                        var forward1IslandId = this.GetIslandId(edge1ForwardId);
-                        if (forward1IslandId == NotSet)
+                        // no access, don't evaluate neighbours.
+                        islandLabels[edgeEnumerator1.Id] = IslandLabels.NoAccess;
+                        continue;
+                    }
+
+                    if (edge1Factor.Direction != 0)
+                    {
+                        // only consider bidirectional edges in this first step.
+                        continue;
+                    }
+
+                    // get label or provisionally set label to own id.
+                    if (!bestLabels.TryGetValue(edgeEnumerator1.Id, out var edge1Label))
+                    {
+                        // label wasn't set yet locally, check globally.
+                        edge1Label = islandLabels[edgeEnumerator1.Id];
+                        if (edge1Label == NotSet)
                         {
-                            if (edge1Factor.Value == 0)
-                            { // no access.
-                                forward1IslandId = NoAccess;
-                                this.SetIslandId(edge1ForwardId, NoAccess);
-                            }
-                            else if ((edge1ForwardId.Forward && edge1Factor.Direction == 2) ||
-                                (!edge1ForwardId.Forward && edge1Factor.Direction == 1))
-                            { // no access in the correct direction.
-                                forward1IslandId = NoAccess;
-                                this.SetIslandId(edge1ForwardId, NoAccess);
-                            }
-                        }
-                        
-                        // evaluate backward.
-                        var backward1IslandId = this.GetIslandId(edge1BackwardId);
-                        if (backward1IslandId == NotSet)
-                        {
-                            if (edge1Factor.Value == 0)
-                            { // no access.
-                                backward1IslandId = NoAccess;
-                                this.SetIslandId(edge1BackwardId, NoAccess);
-                            }
-                            else if ((edge1BackwardId.Forward && edge1Factor.Direction == 2) ||
-                                (!edge1BackwardId.Forward && edge1Factor.Direction == 1))
-                            { // no access in the correct direction.
-                                backward1IslandId = NoAccess;
-                                this.SetIslandId(edge1BackwardId, NoAccess);
-                            }
+                            // provisionally set label to own id.
+                            edge1Label = edgeEnumerator1.Id;
                         }
 
-                        if (backward1IslandId == NoAccess &&
-                            forward1IslandId == NoAccess)
-                        { // no need to check if neighbours need an update here.
+                        bestLabels[edgeEnumerator1.Id] = edge1Label;
+                    }
+
+                    // evaluate neighbours.
+                    edgeEnumerator2.MoveTo(currentVertex);
+                    while (edgeEnumerator2.MoveNext())
+                    {
+                        var edge2Factor = _profile(edgeEnumerator2.Data.Profile);
+                        if (edge2Factor.Value == 0)
+                        {
+                            // should be marked as no access in parent loop.
                             continue;
                         }
 
-                        edgeEnumerator2.Reset();
-                        while (edgeEnumerator2.MoveNext())
+                        if (edge2Factor.Direction != 0)
                         {
-                            var edge2Id = edgeEnumerator2.Id;
-                            if (edge2Id == edge1Id)
-                            { // don't check u-turns.
-                                continue;
+                            // only considder bidirectional edges in this first step.
+                            continue;
+                        }
+
+                        // get label or provisionally set label to own id.
+                        if (!bestLabels.TryGetValue(edgeEnumerator2.Id, out var edge2Label))
+                        {
+                            // label wasn't set locally, check globally.
+                            edge2Label = islandLabels[edgeEnumerator2.Id];
+                            if (edge2Label == NotSet)
+                            {
+                                // provisionally set label to own id.
+                                edge2Label = edgeEnumerator2.Id;
                             }
-                            
-                            var edge2ForwardId = edgeEnumerator2.DirectedEdgeId(); // from current vertex -> neighbour.
-                            var edge2BackwardId = edge2ForwardId.Reverse; // from neighbour -> current vertex.
-                            
-                            // get edge factor.
-                            var edge2Factor = _profile(edgeEnumerator1.Data.Profile);
-                            
-                            // evaluate forward.
-                            var forward2IslandId = this.GetIslandId(edge2ForwardId);
-                            if (forward2IslandId == NotSet)
-                            { // not set yet.
-                                if (edge2Factor.Value == 0)
-                                { // no access.
-                                    forward2IslandId = NoAccess;
-                                    this.SetIslandId(edge2ForwardId, NoAccess);
-                                }
-                                else if ((edge2ForwardId.Forward && edge2Factor.Direction == 2) ||
-                                         (!edge2ForwardId.Forward && edge2Factor.Direction == 1))
-                                { // no access in the correct direction.
-                                    forward2IslandId = NoAccess;
-                                    this.SetIslandId(edge2ForwardId, NoAccess);
-                                }
-                            }
-                            if (forward2IslandId != NoAccess)
-                            { // just update with whatever comes from forward1.
-                                if (forward1IslandId == NoAccess &&
-                                    forward2IslandId == NotSet)
-                                { // start a new island here, the incoming edge has not access.
-                                    forward2IslandId = this.NewIslandId();
-                                }
-                                else if (forward2IslandId == NotSet)
-                                { // take island id from forward1.
-                                    forward2IslandId = forward1IslandId;
-                                }
-                                else
-                                { // connect island1 -> island2.
-                                    forward2IslandId = this.ConnectIslands(forward1IslandId, forward2IslandId);
-                                }
-                                this.SetIslandId(edge2ForwardId, forward2IslandId);
-                            }
-                            
-                            // evaluate backward.
-                            var backward2IslandId = this.GetIslandId(edge2BackwardId);
-                            if (backward2IslandId == NotSet)
-                            { // not set yet.
-                                if (edge2Factor.Value == 0)
-                                { // no access.
-                                    backward2IslandId = NoAccess;
-                                    this.SetIslandId(edge2BackwardId, NoAccess);
-                                }
-                                else if ((edge2BackwardId.Forward && edge2Factor.Direction == 2) ||
-                                         (!edge2BackwardId.Forward && edge2Factor.Direction == 1))
-                                { // no access in the correct direction.
-                                    backward2IslandId = NoAccess;
-                                    this.SetIslandId(edge2BackwardId, NoAccess);
-                                }
-                            }
-                            if (backward2IslandId != NoAccess)
-                            { // just update with whatever comes from backward1.
-                                if (backward2IslandId == NoAccess &&
-                                    backward2IslandId == NotSet)
-                                { // start a new island here, the incoming edge has not access.
-                                    backward2IslandId = this.NewIslandId();
-                                }
-                                else if (backward2IslandId == NotSet)
-                                { // take island id from backward1.
-                                    backward2IslandId = backward2IslandId;
-                                }
-                                else
-                                { // connect island1 -> island2.
-                                    backward2IslandId = this.ConnectIslands(backward2IslandId, backward2IslandId);
-                                }
-                                this.SetIslandId(edge2BackwardId, backward2IslandId);
-                            }
+
+                            bestLabels[edgeEnumerator2.Id] = edge2Label;
+                        }
+
+                        // bidirectional edge, choose best label.
+                        if (edge1Label < edge2Label)
+                        {
+                            bestLabels[edgeEnumerator2.Id] = edge1Label;
+                        }
+                        else
+                        {
+                            bestLabels[edgeEnumerator1.Id] = edge2Label;
+                        }
+                    }
+                }
+
+                // update labels based on best labels.
+                var labelsToUpdate = new HashSet<uint>();
+                foreach (var pair in bestLabels)
+                {
+                    var edgeId = pair.Key; // the edge key.
+                    var label = pair.Value;
+
+                    islandLabels[edgeId] = label;
+                    islandLabels.UpdateLowest(edgeId);
+                }
+            }
+
+            // update all labels to lowest possible.
+            for (uint e = 0; e < islandLabels.Count; e++)
+            {
+                islandLabels.UpdateLowest(e);
+            }
+
+            // NOW ALL LABELLED EDGES ARE AT THEIR BEST.
+            // do the one-way labels.
+            var islandLabelGraph = new IslandLabelGraph();
+
+            currentVertex = 0;
+            while (currentVertex < _network.VertexCount)
+            {
+                if (!edgeEnumerator1.MoveTo(currentVertex))
+                {
+                    continue;
+                }
+
+                // log all connections.
+                while (edgeEnumerator1.MoveNext())
+                {
+                    var edge1Factor = _profile(edgeEnumerator1.Data.Profile);
+                    if (edge1Factor.Value == 0)
+                    {
+                        // no access, don't evaluate neighbours.
+                        islandLabels[edgeEnumerator1.Id] = IslandLabels.NoAccess;
+                        continue;
+                    }
+
+                    if ((edgeEnumerator1.DataInverted && edge1Factor.Direction == 2) ||
+                        !edgeEnumerator1.DataInverted && edge1Factor.Direction == 1)
+                    {
+                        // wrong direction, this edge is oneway away from current vertex.
+                        continue;
+                    }
+
+                    // get label or set to own id.
+                    var edge1Label = islandLabels[edgeEnumerator1.Id];
+                    if (edge1Label == NotSet)
+                    {
+                        edge1Label = edgeEnumerator1.Id;
+                    }
+
+                    // evaluate neighbours.
+                    edgeEnumerator2.MoveTo(currentVertex);
+                    while (edgeEnumerator2.MoveNext())
+                    {
+                        var edge2Factor = _profile(edgeEnumerator2.Data.Profile);
+                        if (edge2Factor.Value == 0)
+                        {
+                            // should be marked as no access in parent loop.
+                            continue;
+                        }
+
+                        if ((edgeEnumerator2.DataInverted && edge2Factor.Direction == 1) ||
+                            !edgeEnumerator2.DataInverted && edge2Factor.Direction == 2)
+                        {
+                            // wrong direction, this edge is oneway away from current vertex.
+                            continue;
+                        }
+
+                        // get label or set to own id.
+                        var edge2Label = islandLabels[edgeEnumerator2.Id];
+                        if (edge2Label == NotSet)
+                        {
+                            edge2Label = edgeEnumerator2.Id;
+                        }
+
+                        if ((edge1Label == edgeEnumerator1.Id &&
+                            edge1Factor.Direction != 0) ||
+                            (edge2Label == edgeEnumerator2.Id &&
+                             edge2Factor.Direction != 0))
+                        { // one of the two is oneway.
+                            islandLabelGraph.Connect(edge1Label, edge2Label);   
                         }
                     }
                 }
---
+++ src/Itinero/Algorithms/Networks/Islands/IslandLabelGraph.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security;
+using Itinero.Algorithms.PriorityQueues;
+using Itinero.Graphs;
+using Itinero.Graphs.Directed;
+
+namespace Itinero.Algorithms.Networks.Islands
+{
+    /// <summary>
+    /// An island label graph.
+    /// </summary>
+    internal class IslandLabelGraph
+    {
+        private readonly Graph _graph;
+
+        /// <summary>
+        /// Creates a new island label graph.
+        /// </summary>
+        public IslandLabelGraph()
+        {
+            _graph = new Graph(1);
+        }
+
+        /// <summary>
+        /// Connects the two given labels.
+        /// </summary>
+        /// <param name="label1">The first label.</param>
+        /// <param name="label2">The second label.</param>
+        public void Connect(uint label1, uint label2)
+        {
+            _graph.UpdateEdgeData();
+            _graph.AddEdge(label1, label2);
+        }
+
+        /// <summary>
+        /// Try to reduce the graph using the given updated labels.
+        /// </summary>
+        /// <param name="updated">The updated labels.</param>
+        public void Reduce(HashSet<uint> updated)
+        {
+            var enumerator = _graph.GetEdgeEnumerator();
+            while (updated.Count > 0)
+            {
+                // do a dykstra search starting at the first.
+                var current = updated.First();
+                var first = current;
+                updated.Remove(current);
+                
+                var settled = new Dictionary<uint, uint>();
+                var queue = new Queue<KeyValuePair<uint, uint>>();
+                queue.Enqueue(new KeyValuePair<uint, uint>(current, Constants.NO_VERTEX));
+
+                var triggeredMerge = false;
+                while (queue.Count > 0)
+                {
+                    // dequeue next.
+                    var dequeued = queue.Dequeue();
+                    current = dequeued.Key;
+                    
+                    if (settled.ContainsKey(current))
+                    { // already settled.
+                        continue;
+                    }
+
+                    // mark as settled.
+                    var previous = dequeued.Value;
+                    settled[current] = previous;
+
+                    if (!enumerator.MoveTo(current))
+                    {
+                        continue;
+                    }
+
+                    while (enumerator.MoveNext())
+                    {
+                        var neighbour = enumerator.Neighbour;
+                        if (settled.ContainsKey(neighbour))
+                        {
+                            continue;
+                        }
+
+                        if (neighbour == first)
+                        { // we found a loop!
+                            var toMerge = new List<uint>();
+                            toMerge.Add(neighbour);
+                            toMerge.Add(current);
+                            while (settled.TryGetValue(current, out previous))
+                            {
+                                if (previous == Constants.NO_VERTEX)
+                                {
+                                    break;;
+                                }
+                                toMerge.Add(previous);
+                                current = previous;
+                            }
+                            
+                            queue.Clear(); // stop the search.
+                            this.Merge(toMerge);
+                            break;
+                        }
+                        
+                        queue.Enqueue(new KeyValuePair<uint, uint>(neighbour, current));
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Merge all the given labels into one.
+        /// </summary>
+        /// <param name="labels">The labels to merge.</param>
+        private void Merge(IEnumerable<uint> labels)
+        {
+            var edgeEnumerator = _graph.GetEdgeEnumerator();
+            var bestLabel = uint.MaxValue;
+            var neighbours = new HashSet<uint>();
+            foreach (var label in labels)
+            {
+                if (label < bestLabel)
+                {
+                    bestLabel = label;
+                }
+
+                if (!edgeEnumerator.MoveTo(label))
+                {
+                    continue;
+                }
+
+                while (edgeEnumerator.MoveNext())
+                {
+                    neighbours.Add(edgeEnumerator.Neighbour);
+                }
+
+                _graph.RemoveEdges(label);
+            }
+
+            if (bestLabel == uint.MaxValue)
+            {
+                return;
+            }
+            
+            foreach (var neighbour in neighbours)
+            {
+                _graph.AddEdge(bestLabel, neighbour);
+
+                
+            }
+        }
+    }
+}
