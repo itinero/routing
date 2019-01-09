ccace1a891da53031654d3b50c52ace4c5ff9714
xivk <ben@itinero.tech>
2019-01-09 15:26:30 +0000

    Enabled all functional tests again.

--- test/Itinero.Test.Functional/Tests/ResolvingTests.cs
+++ test/Itinero.Test.Functional/Tests/ResolvingTests.cs
@@ -38,9 +38,9 @@ namespace Itinero.Test.Functional.Tests
             var router = new Router(routerDb);
 
             GetTestRandomResolves(router, Itinero.Osm.Vehicles.Vehicle.Car.Fastest(), 1000).TestPerf("Random resolves");
-            //GetTestRandomResolvesParallel(router, Itinero.Osm.Vehicles.Vehicle.Car.Fastest(), 1000).TestPerf("Random resolves in parallel");
+            GetTestRandomResolvesParallel(router, Itinero.Osm.Vehicles.Vehicle.Car.Fastest(), 1000).TestPerf("Random resolves in parallel");
             GetTestRandomResolvesCached(router, Itinero.Osm.Vehicles.Vehicle.Car.Fastest(), 1000, 100).TestPerf("Random resolves");
-            //GetTestRandomResolvesCachedParallel(router, Itinero.Osm.Vehicles.Vehicle.Car.Fastest(), 1000).TestPerf("Random resolves in parallel");
+            GetTestRandomResolvesParallelCached(router, Itinero.Osm.Vehicles.Vehicle.Car.Fastest(), 1000, 100).TestPerf("Random resolves in parallel");
         }
 
         /// <summary>
@@ -184,6 +184,62 @@ namespace Itinero.Test.Functional.Tests
                     }
                 });
                 return string.Format("{0}/{1} resolves failed.", errors, vertices.Count);
+            };
+        }
+
+        /// <summary>
+        /// Tests a number of resolves.
+        /// </summary>
+        public static Func<string> GetTestRandomResolvesParallelCached(Router router, Profiles.Profile profile, int count, int unique)
+        {
+            var random = new System.Random();
+            var pool = new List<Coordinate>();
+            
+            // make sure all locations are near to accessible roads.
+            while (pool.Count < unique)
+            {
+                var v1 = (uint)random.Next((int)router.Db.Network.VertexCount);
+                var f1 = router.Db.Network.GetVertex(v1);
+                
+                var factor = router.GetDefaultGetFactor(profile);
+                var edgeEnumerator = router.Db.Network.GetEdgeEnumerator();
+                edgeEnumerator.MoveTo(v1);
+                while (edgeEnumerator.MoveNext())
+                {
+                    var f = factor(edgeEnumerator.Current.Data.Profile);
+                    if (f.Value != 0)
+                    {
+                        pool.Add(router.Db.Network.GetVertex(v1));
+                        break;
+                    }
+                }
+            }
+           
+            var vertices = new List<Coordinate>();
+            while (vertices.Count < count)
+            {
+                var p = random.Next(unique);    
+                vertices.Add(pool[p]);
+            }
+
+            // resolve all.
+            return () =>
+            {
+                router.ResolverCache = new ResolverCache();
+                
+                var errors = 0;
+
+                System.Threading.Tasks.Parallel.ForEach(Enumerable.Range(0, count), (x) =>
+                {
+                    var routerPoint = router.TryResolve(profile, vertices[x]);
+                    if (routerPoint.IsError)
+                    {
+                        errors++;
+                    }
+                });
+                router.ResolverCache = null;
+                
+                return string.Format("{0}/{1} cached resolves failed.", errors, vertices.Count);
             };
         }
     }
